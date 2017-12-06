f5d7721f26bbc89ccfbea3ac96e2ecbf5d1e786b
Woich <minewoichbr@gmail.com>
2017-12-06 14:13:03 -0200

    Tarefa 9

--- src/main/java/Pratica.java
+++ src/main/java/Pratica.java
@@ -2,7 +2,9 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 import utfpr.ct.dainf.pratica.Lancamento;
+import utfpr.ct.dainf.pratica.ProcessaLancamentos;
 
 /**
  * IF62C Fundamentos de Programação 2
@@ -12,6 +14,17 @@ import utfpr.ct.dainf.pratica.Lancamento;
 public class Pratica {
  
     public static void main(String[] args) throws FileNotFoundException, IOException {
+        
+        String path;
+        Scanner scanner = new Scanner(System.in);
+        ProcessaLancamentos lancamentos;
+        List<Lancamento> lancamento;
+        
+        System.out.println("Informe o caminho para o arquivo: ");
+        path = scanner.next();
+        
+        lancamentos = new ProcessaLancamentos(path);
+        lancamento = lancamentos.getLancamentos();
 
     }
     
